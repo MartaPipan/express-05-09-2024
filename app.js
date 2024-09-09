90c0c6565336e849c666f9396d8ff7d49dba2c0f
Marta Prystatska <tradlisboa@gmail.com>
2024-09-09 11:19:24 +0100

    add  *

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log(1);
+const express = require('express');
+//create application - server
+const app = express();
+const PORT = 3000;
+
+//ROUTING
+//app.get()
+app.get('/', (req, res) => {
+    res.send(`<h1>Hi from app! method - ${req.method}, url - ${req.path}</h1>`)
+});
+app.get('*', (req, res) => {
+    res.send(`<h1>404! method - ${req.method}, url - ${req.path}</h1>`)
+});
+
+//app.post()
+//app.put()
+//app.delete()
+
+app.listen(PORT, () => {
+    console.log('app start at port '+PORT);
+});
+
