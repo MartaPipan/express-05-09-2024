a8d738b5cba230c36b175cb813b864d4ecb116f1
Marta Prystatska <tradlisboa@gmail.com>
2024-09-09 23:37:47 +0100

    add middleweres (default export)

--- middlewares/validate.mw.js
+++ middlewares/validate.mw.js
@@ -1,13 +1,13 @@
 const yup = require('yup');
 
-module.exports = validate = async (req, res, next) => {
-    console.log(typeof req.body); // Перевірка типу даних тіла запиту
     const validationSchemaUser = yup.object({
         name: yup.string().trim().required(),
         email: yup.string().trim().required().email(),
         password: yup.string().trim().required(),
         is_male: yup.boolean(),
     });
+
+module.exports = validate = async (req, res, next) => {
     try {
         // Валідація даних і присвоєння валідованого об'єкта назад до req.body
         req.body = await validationSchemaUser.validate(req.body);
